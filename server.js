939d05c83920b1cb24738bba0080ad0cbba38291
sivakumarreddyk <51190442+sivakumarreddyk@users.noreply.github.com>
2019-10-07 13:07:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('<h1 style="color:green;">Java Home Node-app - CI/CD Jenkins!!</h1> \n');
+  res.send('<h1 style="color:blue;">Java Home Node-app - CI/CD integration Jenkins!!</h1> \n');
 });
 
 app.listen(PORT, HOST);
